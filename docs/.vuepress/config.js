0de8b08d329bf01d87d808c8ec7b2cf19a168b26
leich <453180262@qq.com>
2024-02-23 02:05:17 +0800

    初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  base:"/HHGuide/",
+  lang: 'zh-CN',
+  title: '雷小鸿',
+  description: '鸿鸿coding',
+  themeConfig: {
+    nav: [
+      {
+        text: '首页',
+        link: '/'
+      },
+      {
+        text: 'ChatGPT',
+        link: '/chatgpt/'
+      },
+      { 
+         text: '知乎|掘金', 
+         items: [
+                    { text: '知乎', link: 'https://www.zhihu.com/people/lei-xiao-hong-67' },
+                    { text: '掘金', link: 'https://juejin.cn/user/2981531267379101' }
+                ]       
+      }
+    ],
+    sidebar: {
+      '/chatgpt/': [
+        {
+          title: 'ChatGPT',
+          children: ['how-to-update-gpt4']
+        }
+      ]
+    }
+  }
+}
